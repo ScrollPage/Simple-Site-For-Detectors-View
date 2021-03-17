14755243ff98dd0845a00cd8d5a105b3e740108c
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2021-03-17 16:12:29 +0300

    Tcp server

--- backend/service.py
+++ backend/service.py
@@ -1,4 +1,7 @@
 import django
+import json
+import socket
+
 
 from asgiref.sync import sync_to_async
 
@@ -9,6 +12,36 @@ django.setup()
 
 from data.models import Data
 
-@sync_to_async
-def split_json_and_create_data(data):
-    Data.objects.create(**data)
+HEADER = 128
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+def create_data(data, connected=True):
+    try:
+        instance = Data.objects.create(**data)
+    except TypeError as err:
+        print(f'[ERROR] {err}')
+        connected = False
+    except django.db.IntegrityError as err:
+        print(f'[ERROR] {err}')
+        connected = False
+    else:
+        print(f'[SUCCESS] Instance {instance} created')
+    finally:
+        return connected
+
+def convert_to_json(msg, conn, connected=True):
+    try:
+        data = json.loads(msg)
+    except json.JSONDecodeError:
+        connected = False
+        conn.send("Bad msg format".encode(FORMAT))
+        print(f'[ERROR] Could not convert message: {msg} to a JSON')  
+        data = {}
+    else:
+        connected = create_data(data)
+    finally:
+        return connected
---
+++ backend/tcp_client.py
@@ -0,0 +1,19 @@
+import socket
+
+import service as s
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(s.ADDR)
+
+def send(msg):
+    message = msg.encode(s.FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(s.FORMAT)
+    send_length += b' ' * (s.HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(s.FORMAT))
+
+send('{"lightning": 12.3, "humidity": 10.12}')
+
+send(s.DISCONNECT_MESSAGE)
